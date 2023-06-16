c8bc4d1149080f9c31f217ee8d80d12d46763cb5
scohoust <135464867+scohoust@users.noreply.github.com>
2023-06-16 21:56:01 +0100

    Update vancomycin_st.py

--- vancomycin_st.py
+++ vancomycin_st.py
@@ -6,12 +6,13 @@ if 'method' not in st.session_state:
   st.session_state['method'] = 'nil'
 
 st.selectbox('Calculate which dose', ('Loading', 'Maintainence'), key="method")
-
 st.selectbox('Administer by', ('Central', 'Peripheral'), key="route")
 
 crea = st.number_input('Serum creatinine')
 
 if st.session_state.method == 'Maintainence':
   level = st.number_input('Vanc level')
+  infusion = st.select_slider('Current rate', 
+                              options=['1.1', '2.2', '4.2', '6.3', '8.3', '10.4', '12.5'])
             
       
