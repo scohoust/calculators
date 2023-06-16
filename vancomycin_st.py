25f24f3ac59b1808b36eae6c46be8ebd90e3d43c
scohoust <135464867+scohoust@users.noreply.github.com>
2023-06-16 20:43:48 +0100

    Update vancomycin_st.py

--- vancomycin_st.py
+++ vancomycin_st.py
@@ -5,7 +5,7 @@ st.title('Vancomycin calculator')
 if 'method' not in st.session_state:
   st.session_state['method'] = 'nil'
   st.button('Loading', key="method")
-  st.button('Maintainence', key="method"  
+  st.button('Maintainence', key="method")
 else
   st.write(st.session_state.method)
             
