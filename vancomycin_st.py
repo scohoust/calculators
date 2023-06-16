653c31de3e849d141f98a00b4c919d07f5b195f1
scohoust <135464867+scohoust@users.noreply.github.com>
2023-06-16 20:42:49 +0100

    Update vancomycin_st.py

--- vancomycin_st.py
+++ vancomycin_st.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.title('Vancomycin calculator')
 
+if 'method' not in st.session_state:
+  st.session_state['method'] = 'nil'
+  st.button('Loading', key="method")
+  st.button('Maintainence', key="method"  
+else
+  st.write(st.session_state.method)
+            
+            
+      
