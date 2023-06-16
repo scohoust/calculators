60684ed12ed7bbc848b3ed8829bcc45aa2ecd746
scohoust <135464867+scohoust@users.noreply.github.com>
2023-06-16 22:13:47 +0100

    Update vancomycin_st.py

--- vancomycin_st.py
+++ vancomycin_st.py
@@ -8,13 +8,23 @@ if 'method' not in st.session_state:
 st.selectbox('Calculate which dose', ('Loading', 'Maintainence'), key="method")
 st.selectbox('Administer by', ('Central', 'Peripheral'), key="route")
 
-crea = st.number_input('Serum creatinine')
+form = st.form(key="calc")
+crea = form.number_input('Serum creatinine')
+renal = form.checkbox('On haemodialysis')
 
 if st.session_state.method == 'Maintainence':
-  level = st.number_input('Vanc level')
+  level = form.number_input('Vanc level')
   
   if st.session_state.route == 'Central':
-      infusion = st.select_slider('Current rate', 
+      infusion = form.select_slider('Current rate', 
                               options=['1.1', '2.2', '4.2', '6.3', '8.3', '10.4', '12.5'])
-            
+  if st.session_state.route == 'Peripheral':
+      infusion = form.select_slider('Current rate', 
+                              options=['2.1', '4.2', '8.3', '12.5', '16.7', '20.8', '25.0'])            
       
+submitted = form.form_submit_button('Submit')
+
+if not submitted:
+  st.stop();
+
+st.write('Successfully submitted!')
