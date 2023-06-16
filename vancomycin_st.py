f07da5f74c48d7519cabeafe3e19b5c56963357d
scohoust <135464867+scohoust@users.noreply.github.com>
2023-06-16 21:36:09 +0100

    Update vancomycin_st.py

--- vancomycin_st.py
+++ vancomycin_st.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('Vancomycin calculator')
 
 if 'method' not in st.session_state:
-  st.session_state['method'] = ''
+  st.session_state['method'] = 'nil'
 
 st.selectbox('Calculate which dose', ('Loading', 'Maintainence'), key="method")
 
@@ -11,7 +11,7 @@ st.selectbox('Administer by', ('Central', 'Peripheral'), key="route")
 
 crea = st.number_input('Serum creatinine')
 
-if st.session_state.method == "Maintainence":
+if st.session_state.method == 'Maintainence':
   level = st.number_input('Vanc level')
             
       
