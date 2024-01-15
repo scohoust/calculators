b2ce41a7591856ab26c70dcedb4409eb98e26267
scohoust <135464867+scohoust@users.noreply.github.com>
2024-01-15 18:52:52 +0000

    Update vancomycin_st.py

--- vancomycin_st.py
+++ vancomycin_st.py
@@ -119,9 +119,9 @@ if st.session_state.method == 'Loading':
 
         st.write('#### Immediately followed by an continuous infusion:')
         if renal == True or crea >100:
-            st.write('#### *1 g* over 24 hours', route_renal_start)
+            st.info('#### *1 g* over 24 hours {route_renal_start}')
         else:
-            st.write('#### *1.5 g* over 24 hours', route_start_normal)
+            st.info('#### *1.5 g* over 24 hours {route_start_normal}')
     
 if st.session_state.method == 'Maintainence':
     with st.container():
